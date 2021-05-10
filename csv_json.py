55d69af931c9e3d44f4cc1187e9a28ef80fff686
nyccowgirl <nyc_cowgirl@hotmail.com>
2021-05-09 23:26:58 -0500

    modified for usage statement with option flags in command line

--- csv_json.py
+++ csv_json.py
@@ -2,7 +2,7 @@
 Write a CSV-to-JSON translator that expects a path to a CSV file as argument, 
 and for each line, prints out a JSON object encapsulating that record
 
-$ python3 csv_json.py your_file.csv
+$ python3 file.py file.csv [-h] [-d <p | s | sc | t>]
 """
 
 import csv, json, sys
@@ -14,47 +14,74 @@ functions/exception handling and main() as follows:
     reader = csv.reader(open(sys.argv[1], 'r'))
 
 """
-
+# Comment out next two lines for csv with header and simple code
 # reader = csv.DictReader(open(sys.argv[1], 'r', encoding = 'utf-8'))
 # print(json.dumps([row for row in reader], indent = 2))
 
 
 def read_file():
     """Opens the file designated by user and checks whether file name was provided."""
-    
-    if len(sys.argv) < 2:
-        raise(SystemExit)("Filename must be provided on the command line as follows:\n$ python3 this_file.py your_file.csv")
+    # usage statement, with optional flags for header and delimiter 
+    # (limited to common ones for csv)
+    usage = ("usage: " + sys.argv[0] + " csvfile [-h] [-d <p | s | sc | t>)]"
+            "\n\t-h: csv file contains header (default is no header)"
+            "\n\t-d p | s | sc | t: delimiter as p (pipe), s (space), sc (semicolon) or t (tab)"
+            "\n\t(default delimiter is comma)")
 
-    # Checks whether file exists and is csv. If not, program is aborted.
+    # Checks if file is provided. If not, program is aborted.
+    if len(sys.argv) < 2:
+        raise(SystemExit)(f"csv file must be provided in the command line."
+            f"\n{usage}")
+
+    # Checks if file exists and is csv. If not, program is aborted.
     try:
         filename = sys.argv[1]
         if filename.endswith('.csv'):
             csv_file = open(filename, 'r', encoding = 'utf-8')
-        else:
-            raise(SystemExit)(f"The {filename} file is not csv format. Please try another file as follows:\n$ python3 this_file.py your_file.csv")
-    except FileNotFoundError:
-        raise(SystemExit)(f"The {filename} file does not exist. Please try another file as follows:\n$ python3 this_file.py your_file.csv")
 
-    return csv_file
+            # Process optional flags in command line for header and/or delimiter
+            opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+            args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+            header = ('y' if "-h" in opts else 'n')
+
+            try:
+                # Process argument for flag -d
+                if "-d" in opts:
+                    if args[1] == 'p':
+                        separator = '|'
+                    elif args[1] == 's':
+                        separator = ' '
+                    elif args[1] == 'sc':
+                        separator = ';'
+                    elif args[1] == 't':
+                        separator = '\t'
+                else:
+                    separator = ','
+            except:
+                # Program is aborted if flag -d was not followed with valid delimiter
+                raise(SystemExit)(f"-d: No valid delimiter provided"
+                    f"\n{usage}")
+        else:
+            raise(SystemExit)(f"{filename}: No csv file"
+            f"\n{usage}")
+    except FileNotFoundError:
+        raise(SystemExit)(f"{filename}: No such file or directory"
+            f"\n{usage}")
+    return csv_file, separator, header
 
 def translate_csv_json(csvf, delim, head):
     """Reads csv file and prints out each row as JSON object."""
-    # Account for whether csv file has header row. Modify delimiter or header,
-    # if necessary in main().
-    if head == 'n':
-        reader = csv.reader(csvf, delimiter = delim)       # without header row
+    # Accounts for whether csv file has header row and/or modified delimiter.
+    if head == 'y':
+        reader = csv.DictReader(csvf, delimiter = delim)
     else:
-        reader = csv.DictReader(csvf, delimiter = delim)      # with header row
+        reader = csv.reader(csvf, delimiter = delim)
+
     print(json.dumps([row for row in reader], indent = 2))
 
 
 if __name__ == '__main__':
-    f = read_file()
-    # Commented out as course policy would not allow input() function, so modify
-    # delimiter and header, if necessary.
-    # separater = input("Enter the delimiter: ")
-    # header = input("Is there a header row (y/n)? ")
-    separater = ','
-    header = 'y'
-    translate_csv_json(f, separater, header)
+    f, sep, head = read_file()
+    translate_csv_json(f, sep, head)
     f.close()
